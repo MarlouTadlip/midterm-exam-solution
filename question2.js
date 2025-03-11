1e07e5a089adb3104820356c89aa7bebc8a149cd
unknown <YY338@ccs.local>
2025-03-11 05:42:57 -0700

    changes3

--- question2.js
+++ question2.js
@@ -1,4 +1,4 @@
-//Count properties where it takes the keys of the object and returns the length
+//count properties where it takes the keys of the object and returns the length
 const countProperties = (object) => {
     return Object.keys(object).length
 }
