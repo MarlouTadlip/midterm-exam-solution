1430d67c2b17187cc1bd93ab532b31eaa2013b01
unknown <YY338@ccs.local>
2025-03-11 05:41:49 -0700

    changes3

--- question5.js
+++ question5.js
@@ -55,7 +55,6 @@ app.get('/users', async (req, res) => {
     res.json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
